01b27063935023389445adf0f07ff53419e33693
Kerr Travers <vutondesign.github@gmail.com>
2018-01-18 20:02:53 +0000

    Fully add current date feature

--- main.js
+++ main.js
@@ -33,3 +33,11 @@ function preloadImages(){
 }
 preloadImages("favicon.ico");
 
+// Current date feature
+  function showDate() {
+    var d = new Date();
+    var curr_date = d.getDate();
+    var curr_month = d.getMonth() + 1; //months are zero based
+    var curr_year = d.getFullYear();
+    document.write(curr_date + "-" + curr_month + "-" + curr_year);
+  }
